e1bebc7fa6a9a2e1a0a1da76798c550afaad8a54
nraxi <olle.code@hotmail.com>
2023-05-25 01:51:31 +0200

    m

--- frontend-my-examen/src/handlers/Axios.js
+++ frontend-my-examen/src/handlers/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:9000';
+axios.defaults.baseURL = 'http://localhost:9002';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true; //sets cookie inside browser
