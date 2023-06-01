2c35966eb80af917c6ca0daa3fc60c8c2830fab9
nraxi <olle.code@hotmail.com>
2023-06-01 21:23:17 +0200

    first tailwind css for nav done

--- frontend-my-examen/src/App.js
+++ frontend-my-examen/src/App.js
@@ -38,32 +38,35 @@ function App() {
 
 
   return (
-    <div>
-      My page
-      <nav>
-        <div>
-          {jwtToken === "" &&
-            <>
-              <Link to="/">Home</Link><br />
-              <Link to="/signup"><span>Signup</span></Link><br />
-            </>
-          }
-          {jwtToken !== "" &&
-            <>
-              <Link to="/startpage">Startpage</Link><br />
-              <Link to="/manageusers">Manage Users</Link> <br />
-              <Link to="/product">Get from outside api</Link><br />
-              <Link to="/tryme">Json Blob</Link><br />
-            </>
-          }
-          {jwtToken === ""
-            ? <Link to="/login"><span>Login</span></Link>
-            : <Link to="#" onClick={LogOut}><span>Logout</span></Link>
-          }<br />
-
+    <div className="h-screen bg-blue-400">
+      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <h1 className="self-center text-2xl font-semibold whitespace-nowrap">My page</h1>
+          <div className=" w-full md:block md:w-auto" id="navbar-solid-bg">
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 sm:flex-row sm:space-x-8 sm:mt-0 sm:border-0  sm:bg-transparent dark:bg-gray-800  sm:dark:bg-transparent dark:border-gray-700">
+              {jwtToken === "" &&
+                <>
+                  <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/">Home</Link>
+                  <Link className='className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"' to="/signup"><span>Signup</span></Link>
+                </>
+              }
+              {jwtToken !== "" &&
+                <>
+                  <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/startpage">Startpage</Link>
+                  <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/manageusers">Manage Users</Link>
+                  <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/product">Get from outside api</Link>
+                  <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/tryme">Json Blob</Link>
+                </>
+              }
+              {jwtToken === ""
+                ? <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/login"><span>Login</span></Link>
+                : <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="#" onClick={LogOut}><span>Logout</span></Link>
+              }<br />
+            </ul>
+          </div>
         </div>
       </nav >
-      <div>
+      <div >
         <Outlet context={{ jwtToken, setJwtToken }} />
       </div>
     </div >
