66a1d2706a8a881bb1b3fb0394b9d733151d6ffe
nraxi <olle.code@hotmail.com>
2023-06-05 14:06:39 +0200

    first page done

--- frontend-my-examen/src/pages/Home.jsx
+++ frontend-my-examen/src/pages/Home.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
+import palm1Image from '../assets/palm1.png';
+import palm2Image from '../assets/palm2.png';
 
-
-
-
-function Home() {
-
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex justify-between">
+      <img src={palm1Image} alt="Palm 1" className="w-1/4 h-auto pt-20  -mb-96" />
+      <div>
+        <h1 className="text-center mt-80 text-6xl font-bold  bg-gradient-to-l from-teal-200 to-slate-100 bg-clip-text text-transparent pr-2">Welcome to myPortfolio</h1>
+        <p className="mt-4">To see what skills i might have please go to signup</p>
+        <p>enjoy</p>
+      </div>
+      <img src={palm2Image} alt="Palm 1" className="w-1/4 h-auto pt-20  -mb-96"
+      />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
