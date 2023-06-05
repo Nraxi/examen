889e919f69032b1b78b99ebfd819a27a51433eee
nraxi <olle.code@hotmail.com>
2023-06-05 18:41:05 +0200

    fix signup

--- frontend-my-examen/src/pages/Signup.jsx
+++ frontend-my-examen/src/pages/Signup.jsx
@@ -51,57 +51,63 @@ function Signup() {
 
 
   return (
-    <>
-      <div>
-        <h3>Create an account:</h3>
 
-        <div>
-          <Form>
-            <Input
-              type="text"
-              name="name"
-              id="name"
-              ref={nameRef}
-              title="name: "
-              autoComplete="name-new"
-              className="name"
-              onChange={(event) => setName(event.target.value)}
-            />
-            <Input
-              type="text"
-              name="email"
-              id="email"
-              ref={emailRef}
-              title="email: "
-              autoComplete="email-new"
-              className="email"
-              onChange={(event) => setEmail(event.target.value)}
-            />
+    <div className="grid md:grid-cols-3">
+      <h1 className="col-span-3 md:col-start-2 md:col-span-1 text-center mt-10 text-6xl font-bold bg-gradient-to-l from-teal-200 to-slate-100 bg-clip-text text-transparent pr-2">Create your account:</h1>
 
-            <Input
-              type="text"
-              name="password"
-              id="password"
-              ref={passwordRef}
-              title="password: "
-              autoComplete="password"
-              className="password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </Form>
-          <div>
-            <h4> Your inputs:</h4>
-            Name: {name}<br />
-            Email: {email} <br />
-            Password: {password} <br />
-          </div>
-          <br />
-          <span>{message}</span><br />
-          <button onClick={handleSubmit}>Submit</button>
 
+      <div className="mb-2 col-span-3 md:col-start-2 md:col-span-1 row-start-2 p-4 rounded backdrop-blur bg-white/50">
+        <Form >
+          <Input
+            type="text"
+            name="name"
+            id="name"
+            ref={nameRef}
+            title="Name: "
+            autoComplete="name-new"
+            className="p-2 mt-2 ml-8 rounded md:ml-0  lg:ml-10 "
+            onChange={(event) => setName(event.target.value)}
+          />
+          <Input
+            type="text"
+            name="email"
+            id="email"
+            ref={emailRef}
+            title="Email: "
+            autoComplete="email-new"
+            className="p-2 mt-2 ml-9 rounded md:ml-0  lg:ml-11"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          <Input
+            type="text"
+            name="password"
+            id="password"
+            ref={passwordRef}
+            title="Password: "
+            autoComplete="password"
+            className="p-2 mt-2 ml-1 rounded lg:ml-3"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Form>
+        <div className='mt-10'>
+          <h4> Your inputs:</h4>
+          Name: {name}<br />
+          Email: {email} <br />
+          Password: {password} <br />
         </div>
-      </div >
-    </>
+        <br />
+        <span className='mx-auto'>{message}</span><br />
+
+
+
+
+        <button
+          className="mx-20 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-10 border border-green-700 rounded "
+          onClick={handleSubmit}>Submit</button>
+      </div>
+    </div >
+
   )
 }
 
