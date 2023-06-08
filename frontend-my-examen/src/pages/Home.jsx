bc0e90a2c3d08e27d9a363370dce0d94fbfb2e97
nraxi <olle.code@hotmail.com>
2023-06-08 08:44:46 +0200

    fix last min css

--- frontend-my-examen/src/pages/Home.jsx
+++ frontend-my-examen/src/pages/Home.jsx
@@ -4,14 +4,14 @@ import palm2Image from '../assets/palm2.png';
 
 const Home = () => {
   return (
-    <div className="flex justify-between">
-      <img src={palm1Image} alt="Palm 1" className="w-1/4 h-auto pt-20  -mb-96" />
+    <div className="flex sm:justify-between">
+      <img src={palm1Image} alt="Palm 1" className="h-0 w-auto sm:w-1/4 sm:h-auto sm:pt-20  md:mb-0 " />
       <div>
-        <h1 className="text-center mt-80 text-6xl font-bold  bg-gradient-to-l from-teal-200 to-slate-100 bg-clip-text text-transparent pr-2">Welcome to my Portfolio</h1>
+        <h1 className="text-center mt-10 sm:mt-80 text-6xl font-bold  bg-gradient-to-l from-teal-200 to-slate-100 bg-clip-text text-transparent pr-2">Welcome to my Portfolio</h1>
         <p className="mt-4">To see what skills i might have please go to signup</p>
         <p>enjoy</p>
       </div>
-      <img src={palm2Image} alt="Palm 1" className="w-1/4 h-auto pt-20  -mb-96"
+      <img src={palm2Image} alt="Palm 1" className="h-0 w-auto sm:w-1/4 sm:h-auto sm:pt-20  md:mb-0 "
       />
     </div>
   );
