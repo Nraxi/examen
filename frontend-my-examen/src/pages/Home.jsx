40ac747ed4eac5c65dcabb7c7bd579970d727150
nraxi <olle.code@hotmail.com>
2023-06-08 08:48:26 +0200

    change txt in home

--- frontend-my-examen/src/pages/Home.jsx
+++ frontend-my-examen/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <img src={palm1Image} alt="Palm 1" className="h-0 w-auto sm:w-1/4 sm:h-auto sm:pt-20  md:mb-0 " />
       <div>
         <h1 className="text-center mt-10 sm:mt-80 text-6xl font-bold  bg-gradient-to-l from-teal-200 to-slate-100 bg-clip-text text-transparent pr-2">Welcome to my Portfolio</h1>
-        <p className="mt-4">To see what skills i might have please go to signup</p>
+        <p className="mt-4">To see what skills i have, Signup and login</p>
         <p>enjoy</p>
       </div>
       <img src={palm2Image} alt="Palm 1" className="h-0 w-auto sm:w-1/4 sm:h-auto sm:pt-20  md:mb-0 "
